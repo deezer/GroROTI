bbe94117fc252d57278470a7f43ab8a0fb263e84
Denis Germain <dt.germain@gmail.com>
2024-09-19 10:57:45 +0200

    chore: add tracing sdk and add context to webserver

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/deezer/groroti
 
 go 1.22
 
-require github.com/google/uuid v1.3.0
+require github.com/google/uuid v1.6.0
 
 require (
 	github.com/BurntSushi/toml v1.2.0
@@ -11,21 +11,30 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.15
 	github.com/prometheus/client_golang v1.19.0
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.55.0
+	go.opentelemetry.io/otel v1.30.0
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.30.0
+	go.opentelemetry.io/otel/sdk v1.30.0
 	golang.org/x/image v0.15.0
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.48.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
+	go.opentelemetry.io/otel/metric v1.30.0 // indirect
+	go.opentelemetry.io/otel/trace v1.30.0 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
 )
 
 require (
 	github.com/rs/zerolog v1.30.0
-	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
 	"github.com/deezer/groroti/internal/model"
-	"github.com/deezer/groroti/internal/server"
 	"github.com/deezer/groroti/internal/services"
 	"github.com/deezer/groroti/internal/staticEmbed"
 	"github.com/rs/zerolog/log"
@@ -11,20 +19,74 @@ import (
 var Version string
 
 func main() {
+	if err := run(); err != nil {
+		log.Fatal().Msgf(err.Error())
+	}
+}
+
+func run() (err error) {
+	// Handle SIGINT (CTRL+C) gracefully.
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	// Set up OpenTelemetry.
+	otelShutdown, err := services.SetupOTelSDK(ctx)
+	if err != nil {
+		return
+	}
+	// Handle shutdown properly so nothing leaks.
+	defer func() {
+		err = errors.Join(err, otelShutdown(context.Background()))
+	}()
+
 	log.Info().Msgf("GroROTI version v%s", Version)
 	sqliteDatabase := model.InitDatabase()
 	defer sqliteDatabase.Close()
 
 	// load embedded gotemplates in embed.Templates
-	err := staticEmbed.LoadTemplates()
+	err = staticEmbed.LoadTemplates()
 	if err != nil {
-		log.Fatal().Msgf("Couldn't load templates : %s", err.Error())
+		return fmt.Errorf("Couldn't load templates : %s", err.Error())
 	}
 
 	services.Version = Version
 	services.Register()
-	if err := server.Start(); err != nil {
-		log.Fatal().AnErr("The server couldn't start", err)
+
+	configRepository, err := services.GetConfig()
+	if err != nil {
+		return err
+	}
+	addr := configRepository.BuildServerAddr()
+
+	log.Info().Msgf("Start listening on %s", addr)
+	http.ListenAndServe(addr, nil)
+
+	// Start HTTP server.
+	srv := &http.Server{
+		Addr:         addr,
+		BaseContext:  func(_ net.Listener) context.Context { return ctx },
+		ReadTimeout:  time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	srvErr := make(chan error, 1)
+
+	go func() {
+		srvErr <- srv.ListenAndServe()
+	}()
+
+
+	// Wait for interruption.
+	select {
+	case err = <-srvErr:
+		// Error when starting HTTP server.
+		return
+	case <-ctx.Done():
+		// Wait for first CTRL+C.
+		// Stop receiving signal notifications as soon as possible.
+		stop()
 	}
 
+	// When Shutdown is called, ListenAndServe immediately returns ErrServerClosed.
+	err = srv.Shutdown(context.Background())
+	return
 }
